7568b2fa0e5943a38929629ad41710943005b9ac
said-ml <113719075+said-ml@users.noreply.github.com>
2024-02-18 04:03:26 -1100

    devide and conquer algorithm

--- devide_and_conquer.py
+++ devide_and_conquer.py
@@ -5,6 +5,30 @@ from typing import Union
 from typing import Callable
 from typing import Optional
 
+def merge(nums1, nums2):
+    # List to restore results
+    merged=[]
+    i, j=0,0
+    # loops over the two lists
+    while i<len(nums1) and j<len(nums2):
+
+          if nums1[i]<nums2[j]:
+              merged.append(nums1[i])
+              # increment i
+              i+=1
+
+          else :
+              merged.append(nums2[j])
+              # increment i
+              j+=1
+
+    # Getting the remai ing parts
+    nums1_tails=nums1[i:]
+    nums2_tails = nums2[j:]
+
+    return merged+nums1_tails+nums2_tails
+
+
 def  merge_sort(nums:Optional[List[int]])->Optional[List[int]]:
 
      if len(nums)==0:
